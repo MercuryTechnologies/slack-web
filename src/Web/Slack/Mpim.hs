2facd0ff729dd916dfb1a287407a5fd2ae113258
Emmanuel Touzery <etouzery@gmail.com>
2017-06-22 21:19:08 +0200

    basic support for mpim.list + mpim.history

---
+++ src/Web/Slack/Mpim.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+
+----------------------------------------------------------------------
+-- |
+-- Module: Web.Slack.Mpim
+-- Description:
+--
+--
+--
+----------------------------------------------------------------------
+
+module Web.Slack.Mpim
+  ( Group(..)
+  , ListRsp(..)
+  )
+  where
+
+-- aeson
+import Data.Aeson.TH
+
+-- base
+import GHC.Generics (Generic)
+
+-- slack-web
+import Web.Slack.Util
+
+-- text
+import Data.Text (Text)
+
+-- time
+import Data.Time.Clock.POSIX
+
+data Group =
+  Group
+    { groupId :: Text
+    , groupName :: Text
+    , groupIsMpim :: Bool
+    , groupCreated :: POSIXTime
+    , groupCreator :: Text
+    , groupIsArchived :: Bool
+    , groupMembers :: [Text]
+    }
+  deriving (Eq, Generic, Show)
+
+$(deriveJSON (jsonOpts "group") ''Group)
+
+data ListRsp =
+  ListRsp
+    { listRspOk :: Bool
+    , listRspGroups :: [Group]
+    }
+  deriving (Eq, Generic, Show)
+
+$(deriveFromJSON (jsonOpts "listRsp") ''ListRsp)
