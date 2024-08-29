872e956b1cbd5d5f263be055f3905051a731624b
Lev Dubinets <3114081+ldub@users.noreply.github.com>
2024-08-29 11:54:32 -0700

    add since

--- src/Web/Slack/Experimental/Events/Types.hs
+++ src/Web/Slack/Experimental/Events/Types.hs
@@ -56,6 +56,8 @@ $(deriveFromJSON snakeCaseOptions ''MessageEvent)
 -- | <https://api.slack.com/events/message/bot_message>
 -- This is similar to a MessageEvent but sent by a bot, for example
 -- messages that Reacji Channeler sends.
+--
+-- @since 2.0.0.2
 data BotMessageEvent = BotMessageEvent
   { blocks :: Maybe [SlackBlock]
   , channel :: ConversationId
