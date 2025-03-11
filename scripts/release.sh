52d796a38da4360da37703c13af2185cd53bdf8a
Jade Lovelace <jadel@mercury.com>
2025-03-10 17:39:41 -0700

    feat: release script
    We should automate this more so that it is harder to *cough* screw up
    git tags. Not that I would have any idea about that. Oops.

---
+++ scripts/release.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -xeu
+
+version=$(perl -ne '/^version: (.+)$/ && print "$1\n"' slack-web.cabal)
+git fetch origin
+
+# Ensure we are on origin/master
+[[ $(git rev-parse origin/master) == $(git rev-parse HEAD) ]] || (echo "not up to date"; exit 1)
+
+# Verify Nix
+nix flake check -Lv
+
+cabal sdist
+
+sdist="dist-newstyle/sdist/slack-web-${version}.tar.gz"
+tar tf "$sdist"
+
+echo -e "\n\nWould you like to release this sdist as $version? Enter the version number if so."
+read resp
+
+[[ "$resp" == "$version" ]]
+
+cabal upload "$sdist"
+git tag "v${version}"
+git push origin "v${version}"
